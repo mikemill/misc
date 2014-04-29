dec0def8270ff94e4b42d9836dbf097c9e52454a
Michael Miller <mikemill@gmail.com>
2014-04-29 14:11:58 -0700

    Add comments and clean up formatting

--- basicquest/game.rb
+++ basicquest/game.rb
@@ -3,10 +3,14 @@ require 'json'
 require 'highline/import'
 
 class Map
+    # Map of the rooms and their interconnection
+
     @@directions = ['north', 'east', 'south', 'west']
+
     attr_reader :rooms, :target, :shortest_paths
 
     def initialize
+        # Load the rooms from the config file and choose a target
         rooms = JSON.load(File.open('rooms.json'))
 
         rooms.each do |room, connections|
@@ -24,19 +28,22 @@ class Map
     end
 
     def move (current_room, direction)
+        # Move in a particular direction and return the new room (or nil if unable to move)
         room = @rooms[current_room]
         if room[direction]
             return room[direction]
         else
-            return nil 
+            return nil
         end
     end
 
     def randomRoom (exclude=nil)
+        # Pick a random room excluding the one the actor is in
         return @rooms.keys.reject{|r| r == exclude}.sample
     end
 
     def enterRoom (room)
+        # Enter a room and display a message about anything special in there
         if room == @target
             puts "There is a glowing dias in the center of the room"
         end
@@ -47,6 +54,7 @@ class Map
     end
 
     def findShortPaths
+        # Pre-calculate the direction Grue needs to move from any room to a target room
         paths = Hash[rooms.keys.collect{|v| [v, {}]}]
 
         queue = []
@@ -59,6 +67,7 @@ class Map
             end
         end
 
+        # This is essentially a breadth-first search
         while queue.length > 0
             name, origin, origin_dir = queue.shift
             room = @rooms[name]
@@ -86,11 +95,12 @@ class Player
     def initialize (map)
         @map = map
         @gems = 0
+        @room = nil
         spawn
     end
 
     def spawn
-        @room = nil
+        # Spawn the player in a room that they aren't currently in
         @room = @map.randomRoom @room
         print "You have spawned in the ", @room, " room\n"
     end
@@ -117,15 +127,18 @@ class Grue
     end
 
     def spawn (player)
+        # Based on the player's position spawn Grue in the far away room
         @room = @map.rooms[player.room]['spawn']
     end
 
-    def move 
+    def move
+        # Grue always takes the shortest path.
         direction = @map.shortest_paths[@room][@player.room]
         @room = @map.move @room, direction
     end
 
     def flee
+        # The player found Grue and now Grue must flee to a random room
         room = @map.rooms[@room]
 
         @map.directions.shuffle.each do |direction|
@@ -148,30 +161,32 @@ class Game
         player = Player.new map
         grue = Grue.new map, player
 
+        # Build a regex of allowed inputs for the direction
         directions_regex = map.directions.join('|')
         directions_regex = /#{directions_regex}/i
 
+        # Main logic loop
         loop do
             3.times do
-               direction = ask("Direction? ") { |prompt|
-                   prompt.validate = directions_regex
-                   prompt.responses[:not_valid] = "Direction must be one of " + map.directions.join(' ')
-               }
+                direction = ask("Direction? ") { |prompt|
+                    prompt.validate = directions_regex
+                    prompt.responses[:not_valid] = "Direction must be one of " + map.directions.join(' ')
+                }
 
-               player.move direction.downcase
+                player.move direction.downcase
 
-               if player.room == grue.room
-                   grue.flee
-                   player.gems += 1
-                   puts "You have found a gem!  You know have %d gems." % player.gems
-                   if player.gems == TARGET_GEM_COUNT
-                       puts "You have enough gems to leave.  You should find the dias before the Grue finds you!"
+                if player.room == grue.room
+                    grue.flee
+                    player.gems += 1
+                    puts "You have found a gem!  You know have %d gems." % player.gems
+                    if player.gems == TARGET_GEM_COUNT
+                        puts "You have enough gems to leave.  You should find the dias before the Grue finds you!"
                     end
-               end
+                end
 
-               if player.room == map.target and player.gems == TARGET_GEM_COUNT
-                   puts "You put the gems in the dias and safely teleport away"
-                   return
+                if player.room == map.target and player.gems == TARGET_GEM_COUNT
+                    puts "You put the gems in the dias and safely teleport away"
+                    return
                 end
             end
 
